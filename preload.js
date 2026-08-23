707697c590eb850be701966a17f4acd70cac35c9
itsjustsomecoolcode19393 <noahhogeweg@outlook.com>
2026-08-23 22:06:17 +0200

    Add preload script for security

---
+++ preload.js
@@ -0,0 +1,10 @@
+// Preload script for security
+// Uncomment if you need to expose specific APIs to the renderer process
+
+/*
+const { contextBridge, ipcMain } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Add any exposed APIs here
+});
+*/
